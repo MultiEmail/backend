33e2f95ee4d4de531b52dabb9891a8c6ea2a67ec
DaatUserName <hopetobylol@gmail.com>
2022-10-01 15:56:19 +0200

    Admin from terminal

---
+++ src/utils/admin.terminal.ts
@@ -0,0 +1,34 @@
+import yargs from 'yargs';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import { exit } from 'process';
+import UserModel from '../models/user.model';
+
+const { email, username, password } = yargs(process.argv.slice(3))
+  .usage('admin -e <email> -u <username> -p <password>')
+  .options({
+    email: { type: 'string', demandOption: true, alias: 'e' },
+    username: { type: 'string', demandOption: true, alias: 'u' },
+    password: { type: 'string', demandOption: true, alias: 'p' },
+  })
+  .parseSync();
+
+mongoose.connect(process.env.DB_URI as string, () => {
+  UserModel.create({
+    role: 'admin',
+    username,
+    email,
+    password,
+  })
+    .then(() => {
+      console.log('✔ Admin created successfully');
+
+      exit(0);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err) console.log('❌ Admin user already exists');
+
+      exit(1);
+    });
+});
