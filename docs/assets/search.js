6e3e022271e3daab7938a96f1baf8d83ba40bc22
Ayush Chugh <ayushchugh2006@gmail.com>
2022-10-03 18:09:54 +0530

    ci: :construction_worker: deploy docs to github pages

--- docs/assets/search.js
+++
@@ -1,1 +0,0 @@
-window.searchData = JSON.parse("{\"kinds\":{},\"rows\":[],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[],\"invertedIndex\":[],\"pipeline\":[]}}");
