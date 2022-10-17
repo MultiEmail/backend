e46fdec564a8eb1072f7076e80fc49bfe8466890
Tharun K <53267275+tharun634@users.noreply.github.com>
2022-10-18 01:10:57 +0530

    feat: send marketing emails (#133)
    * feat: send marketing emails
    
    * fix: fix docstrings

--- src/schemas/marketingEmail.schema.ts
+++ src/schemas/marketingEmail.schema.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 /**
  * This schema is used to validate `POST /marketing` request
  *
- * @constant
  * @author tharun634
  */
 export const createMarketingEmailSchema = z.object({
@@ -20,3 +19,24 @@ export const createMarketingEmailSchema = z.object({
  * @author tharun634
  */
 export type CreateMarketingEmailSchema = z.TypeOf<typeof createMarketingEmailSchema>;
+
+/**
+ * This schema is used to validate `POST /marketing-email` request
+ *
+ * @author tharun634
+ */
+export const sendMarketingEmailSchema = z.object({
+	body: z.object({
+		html: z.string({ required_error: "html is required" }),
+		subject: z.string({ required_error: "subject is required" }),
+	}),
+});
+
+/**
+ * This type is generated using sendMarketingEmailSchema and can be used
+ * as express Request type generic
+ *
+ * @author tharun634
+ */
+
+export type SendMarketingEmailSchema = z.TypeOf<typeof sendMarketingEmailSchema>;
--- src/services/marketingEmail.service.ts
+++ src/services/marketingEmail.service.ts
@@ -1,5 +1,8 @@
 import { DocumentDefinition } from "mongoose";
 import MarketingEmailModel, { MarketingEmail } from "../models/marketingEmail.model";
+import { User } from "../models/user.model";
+import { sendEmail } from "../utils/email.util";
+import { findUsersService } from "./user.service";
 
 /**
  * Create marketing email in database
@@ -9,4 +12,19 @@ import MarketingEmailModel, { MarketingEmail } from "../models/marketingEmail.mo
  */
 export function createMarketingEmailService(payload: DocumentDefinition<MarketingEmail>) {
 	return MarketingEmailModel.create(payload);
+}
+
+/**
+ * Send marketing emails to users who have opted in to receive marketing emails.
+ * @param {string} html - string - The HTML content of the email
+ * @param {string} subject - The subject of the email
+ *
+ * @author tharun634
+ */
+export function sendMarketingEmailService(html: string, subject: string) {
+	const query = findUsersService({ receiveMarketingEmails: true });
+	query.select("email");
+	return query.exec().then((users: User[]) => {
+		users.map(async (user) => await sendEmail(user.email, subject, html));
+	});
 }
