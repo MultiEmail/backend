5b8d470db2c06a673fb09c595070fb61a418c485
Tharun K <53267275+tharun634@users.noreply.github.com>
2022-10-20 17:35:44 +0530

    feat: mark user as admin (#146)
    * feat: mark user as admin
    
    * feat: mark user as admin
    
    * fix: refactor + tests
    
    * refactor: `patchMarkUserVerifiedHandler`
    
    - moved to `user.controller.ts`

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { PatchMarkUserAdminSchema, PatchMarkUserVerifiedSchema } from "../schemas/admin.schema";
 
 import { PatchUserSchema } from "../schemas/user.schema";
-import {
-	findUserByUsernameService,
-	updateUserByIdService,
-} from "../services/user.service";
+import { findUserByUsernameService, updateUserByIdService } from "../services/user.service";
 import logger from "../utils/logger.util";
-
 
 /**
  * This controller will update user's username
@@ -45,6 +42,78 @@ export const patchUserHandler = async (
 
 		return res.status(StatusCodes.OK).json({
 			message: "User updated successfully",
+		});
+	} catch (err) {
+		logger.error(err);
+
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+			error: "Internal Server Error",
+		});
+	}
+};
+
+/**
+ * This controller will mark user as verified.
+ * this can be used by admin to mark any user as verified
+ *
+ * @param req request
+ * @param res response
+ *
+ * @author aayushchugh, is-it-ayush
+ */
+export const patchMarkUserVerifiedHandler = async (
+	req: Request<PatchMarkUserVerifiedSchema["params"]>,
+	res: Response,
+) => {
+	const { id } = req.params;
+
+	try {
+		const verifiedUser = await updateUserByIdService(id, { verified: true });
+
+		if (!verifiedUser) {
+			return res.status(StatusCodes.NOT_FOUND).json({
+				error: "User not found",
+			});
+		}
+
+		return res.status(StatusCodes.OK).json({
+			message: "User verified successfully",
+		});
+	} catch (err) {
+		logger.error(err);
+
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+			error: "Internal Server Error",
+		});
+	}
+};
+
+/**
+ * This controller will mark user as admin.
+ * this can be used by admin to mark any user as admin
+ *
+ * @param req request
+ * @param res response
+ *
+ * @author tharun634
+ */
+export const patchMarkUserAdminHandler = async (
+	req: Request<PatchMarkUserAdminSchema["params"]>,
+	res: Response,
+) => {
+	const { id } = req.params;
+
+	try {
+		const markedAdminUser = await updateUserByIdService(id, { role: "admin" });
+
+		if (!markedAdminUser) {
+			return res.status(StatusCodes.NOT_FOUND).json({
+				error: "User not found",
+			});
+		}
+
+		return res.status(StatusCodes.OK).json({
+			message: "User marked as admin successfully",
 		});
 	} catch (err) {
 		logger.error(err);
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import { deleteUserHandler, getAllUsersHandler } from "../controllers/admin.controller";
 import {
-    getAllUsersHandler,
+	patchMarkUserAdminHandler,
 	patchMarkUserVerifiedHandler,
-	deleteUserHandler,
-} from "../controllers/admin.controller";
+} from "../controllers/user.controller";
 import deserializeUser from "../middleware/deserializeUser.middleware";
 import requireAdminRole from "../middleware/requireAdminRole.middleware";
 import validateRequest from "../middleware/validateRequest.middleware";
 import {
 	deleteUserSchema,
+	patchMarkUserAdminSchema,
 	patchMarkUserVerifiedSchema,
 } from "../schemas/admin.schema";
 
@@ -19,26 +20,53 @@ const adminRouter: Router = Router();
 /**
  * This route will get all users
  * protected for admin
- * 
+ *
  * @author aayushchugh, is-it-ayush
  */
- adminRouter.get("/admin/users", deserializeUser, requireAdminRole, getAllUsersHandler);
- 
- 
-/** 
+adminRouter.get("/admin/users", deserializeUser, requireAdminRole, getAllUsersHandler);
+
+/**
  * This route will mark user as verified.
  * protected for admin
- * 
+ *
  * @author aayushchugh, is-it-ayush
-*/
- adminRouter.patch("/admin/users/markverified/:id", deserializeUser, requireAdminRole, validateRequest(patchMarkUserVerifiedSchema), patchMarkUserVerifiedHandler);
+ */
+adminRouter.patch(
+	"/admin/users/markverified/:id",
+	deserializeUser,
+	requireAdminRole,
+	validateRequest(patchMarkUserVerifiedSchema),
+	patchMarkUserVerifiedHandler,
+);
 
 /**
  * This route will delete a user
  * protected for admin
- * 
+ *
  * @author aayushchugh, is-it-ayush
  */
- adminRouter.route("/admin/users/:id").delete(deserializeUser, requireAdminRole, validateRequest(deleteUserSchema), deleteUserHandler);
+adminRouter
+	.route("/admin/users/:id")
+	.delete(
+		deserializeUser,
+		requireAdminRole,
+		validateRequest(deleteUserSchema),
+		deleteUserHandler,
+	);
 
- export default adminRouter;
+/**
+ * This route does following things
+ * PATCH -> mark user as admin
+ *
+ * @author tharun634
+ */
+adminRouter
+	.route("/admin/users/markadmin/:id")
+	.patch(
+		validateRequest(patchMarkUserAdminSchema),
+		deserializeUser,
+		requireAdminRole,
+		patchMarkUserAdminHandler,
+	);
+
+export default adminRouter;
