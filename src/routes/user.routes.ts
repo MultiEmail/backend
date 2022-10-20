06e0a76bcc1015535f22d21470b5fba7469b0d68
tharun634 <tharun634@gmail.com>
2022-10-20 16:12:12 +0530

    feat: mark user as admin

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { patchUserHandler } from "../controllers/user.controller";
+import { patchMarkUserAdminHandler, patchUserHandler } from "../controllers/user.controller";
 import deserializeUser from "../middleware/deserializeUser.middleware";
+import requireAdminRole from "../middleware/requireAdminRole.middleware";
 import requireSameUser from "../middleware/requireSameUser.middleware";
 import validateRequest from "../middleware/validateRequest.middleware";
-import { patchUserSchema } from "../schemas/user.schema";
+import { patchMarkUserAdminSchema, patchUserSchema } from "../schemas/user.schema";
 
 const userRouter = Router();
 
@@ -18,5 +19,20 @@ const userRouter = Router();
 userRouter
 	.route("/users/:id")
 	.patch(validateRequest(patchUserSchema), deserializeUser, requireSameUser, patchUserHandler);
+
+/**
+ * This route does following things
+ * PATCH -> mark user as admin
+ *
+ * @author tharun634
+ */
+userRouter
+	.route("/admin/users/markadmin/:id")
+	.patch(
+		validateRequest(patchMarkUserAdminSchema),
+		deserializeUser,
+		requireAdminRole,
+		patchMarkUserAdminHandler,
+	);
 
 export default userRouter;
