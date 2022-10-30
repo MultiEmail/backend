10f81b58826db1bdb9f8b85dbf26d4032da89a0c
bhatti <bhattiarbazparis@gmail.com>
2022-10-30 18:45:31 +0100

    :sparkles: Add author

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -190,6 +190,14 @@ export const patchMarkUserAdminHandler = async (
 	}
 };
 
+/**
+ * This controller will unscribe user marketing mail.
+ *
+ * @param req request
+ * @param res response
+ *
+ * @author arbaz23
+ */
 export const updateUnsubcribeUser = async (
 	req: Request<PatchUserSchema["params"]>,
 	res: Response,
